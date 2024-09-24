1e71610c75f8d90c2fc6657a0b3eff3c704d3143
Rutu1130 <154366245+Rutu1130@users.noreply.github.com>
2024-09-24 21:50:48 +0530

    Add files via upload

---
+++ Problems on Patterns/Program9.py
@@ -0,0 +1,20 @@
+# Python Program to Print X Star Pattern
+#Enter X Pattern Odd Rows = 5
+#X Star Pattern
+#   *   *
+#    * * 
+#     *  
+#    * * 
+#   *   *
+ 
+rows = int(input("Enter X Pattern Odd Rows = "))
+
+print("X Star Pattern") 
+
+for i in range(0, rows):
+    for j in range(0, rows):
+        if(i == j or j == rows - 1 - i):
+            print('*', end = '')
+        else:
+            print(' ', end = '')
+    print()
