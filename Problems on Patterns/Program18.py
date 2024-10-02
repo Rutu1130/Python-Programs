b3a38329d99c687bcdde7d5a4ad95e7b0dab403e
Rutu1130 <154366245+Rutu1130@users.noreply.github.com>
2024-10-02 22:41:15 +0530

    Add files via upload

---
+++ Problems on Patterns/Program18.py
@@ -0,0 +1,14 @@
+
+#    5 5 5 5 5 
+#    4 4 4 4 
+#    3 3 3 
+#    2 2 
+#    1 
+
+rows = 5
+# reverse loop
+for i in range(rows, 0, -1):
+    num = i
+    for j in range(0, i):
+        print(num, end=' ')
+    print("\r")
